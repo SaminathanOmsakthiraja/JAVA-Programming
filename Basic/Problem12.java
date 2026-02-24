70976dca060968058c2316c2b900c9c0dbc1716e
OM.Saminathan <omsaminathan@gmail.com>
2026-02-24 18:34:23 +0530

    Cinema Theatre Collection Report

---
+++ Basic/Problem12.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tkt = sc.nextInt();
+        int snk = sc.nextInt();
+        int mnt = sc.nextInt();
+        int elc = sc.nextInt();
+        int value = tkt + snk - mnt - elc;
+        System.out.println (value);
+        sc.close();
+    }
+}
