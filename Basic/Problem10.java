aedfb0ef26e9de2f8c6e85b1cf0a3ff46964f39d
OM.Saminathan <omsaminathan@gmail.com>
2026-02-24 18:29:30 +0530

    Delivery Service Earnings Calculator

---
+++ Basic/Problem10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int deliv = sc.nextInt();
+        int payot = sc.nextInt();
+        int incnt = sc.nextInt();
+        int flcst = sc.nextInt();
+        int value = (deliv * payot) + incnt - flcst;
+        System.out.println (value);
+        sc.close();
+    }
+}
