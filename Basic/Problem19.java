09e15804ba3928a202d56aa482ed9a6ee4184f55
OM.Saminathan <omsaminathan@gmail.com>
2026-02-25 18:59:23 +0530

    Car Rental Company Profit Calculator

---
+++ Basic/Problem19.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Problem19{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int days = sc.nextInt();
+        int rent = sc.nextInt();
+        int late = sc.nextInt();
+        int fuel = sc.nextInt();
+        int maintenance = sc.nextInt();
+        int value = (days * rent) + late - fuel - maintenance;
+        System.out.println (value);
+        sc.close();
+    }
+}
