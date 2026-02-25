51bd72190d6062ea24567b1cebe6081378acb849
OM.Saminathan <omsaminathan@gmail.com>
2026-02-25 18:56:21 +0530

    College Event Fund Management

---
+++ Basic/Problem18.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Problem18{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int registration = sc.nextInt();
+        int sponsorship = sc.nextInt();
+        int stall = sc.nextInt();
+        int stage = sc.nextInt();
+        int celebrity = sc.nextInt();
+        int marketing = sc.nextInt();
+        int value = registration + sponsorship + stall - stage - celebrity - marketing;
+        System.out.println (value);
+        sc.close();
+    }
+}
