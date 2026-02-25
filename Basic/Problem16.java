681a7f95320236f87da0e667a850231f17d5c025
OM.Saminathan <omsaminathan@gmail.com>
2026-02-25 18:35:19 +0530

    Railway Ticket Revenue System

---
+++ Basic/Problem16.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Problem16{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int genPass = sc.nextInt();
+        int genFare = sc.nextInt();
+        int acPass = sc.nextInt();
+        int acFare = sc.nextInt();
+        int platform = sc.nextInt();
+        int maintenance = sc.nextInt();
+        int fuel = sc.nextInt();
+        int value = (genPass * genFare) + (acPass * acFare) + platform - maintenance - fuel;
+        System.out.println (value);
+        sc.close();
+    }
+}
