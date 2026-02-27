e5e9d2b2ba4c214ced420f601892ca074d700e7e
OM.Saminathan <omsaminathan@gmail.com>
2026-02-27 13:31:13 +0530

    Healthcare Insurance Premium Calculator

--- Conditional Statement/Problem3.java
+++ Conditional Statement/Problem3.java
@@ -3,14 +3,12 @@ public class Problem3{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
-        int gp=0;
         String smksts = sc.next();
         String preexisting = sc.next();
         String coverage = sc.next();
         double base = 0,risk = 0,total = 0;
 
         if (age >=18 && age <=30){
-            gp = 1;
             if (coverage.equals("Basic"))
                 base = 200;
             else if(coverage.equals("Standard"))
@@ -19,7 +17,6 @@ public class Problem3{
                 base = 500;
         }
         else if (age >30 && age <=50){
-            gp = 2;
             if (coverage.equals("Basic"))
                 base = 250;
             else if(coverage.equals("Standard"))
@@ -28,7 +25,6 @@ public class Problem3{
                 base = 500;
         }
         else if (age >50 && age <=65){
-            gp = 3;
             if (coverage.equals("Basic"))
                 base = 350;
             else if(coverage.equals("Standard"))
@@ -37,7 +33,6 @@ public class Problem3{
                 base = 700;
         }
         else if (age>65){
-            gp = 4;
             if (coverage.equals("Basic"))
                 base = 400;
             else if(coverage.equals("Standard"))
