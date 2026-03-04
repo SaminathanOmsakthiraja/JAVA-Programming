1091c897310b37f367e4dab9cb332588c92a9a28
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-03-04 15:28:10 +0530

    updated

--- Basic/Problem10.java
+++ Basic/Problem10.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class Problem10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int deliv = sc.nextInt();
--- Basic/Problem12.java
+++ Basic/Problem12.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class Problem12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int tkt = sc.nextInt();
