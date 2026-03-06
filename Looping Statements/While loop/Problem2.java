51037b2d0f0bde2c546961d15cb21ba64003ebef
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-03-06 12:06:43 +0530

    Sum Calculator Until Zero

--- Looping Statements/While loop/Problem2.java
+++ Looping Statements/While loop/Problem2.java
@@ -4,14 +4,16 @@ public class Problem2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = 1, sum = 0;
+        int n = 1, sum = 0, count = 0;
         while (n!=0) {
             System.out.println("Enter number (0 to stop): ");
             n = sc.nextInt();
             sum += n;
+            count++;
         }
 
         System.out.println("Total sum: " + sum);
+        System.out.println("Count: " + (count-1));
 
         sc.close();
     }    
