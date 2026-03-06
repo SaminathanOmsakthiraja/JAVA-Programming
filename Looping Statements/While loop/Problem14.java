3fe78f1bf0b8cc43d15e854467e0b8fc9ebe5784
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-03-06 18:59:05 +0530

    Armstrong Number Checker

---
+++ Looping Statements/While loop/Problem14.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Problem14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int dp1 = n,dp2 = n;
+        int count = 0;
+        while (dp1 != 0) {
+            count++;
+            dp1 /= 10;
+        }
+        int sum = 0;
+        while (dp2 != 0) {
+            sum += Math.pow(dp2 % 10,count);
+            dp2 /= 10;
+        }
+
+        if (sum == n)
+            System.out.println(n + " is an Armstrong number");
+        else
+            System.out.println(n + " is not an Armstrong number");
+
+        sc.close();
+    }   
+}
