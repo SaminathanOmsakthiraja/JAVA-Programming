12a79d8133a732dc30efcc57da2249edcadee375
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-03-09 13:11:29 +0530

    Calendar Month Grid Generator

---
+++ Looping Statements/Nested For loop/Problem5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int days = sc.nextInt();
+        int mon_start = sc.nextInt();
+
+        int cells = mon_start - 1 + days;
+
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun ");
+
+        int day=1;
+        for (int i=1 ; i<=cells-days ; i++){
+            int point = 1;
+            if (i==1){
+                for (int k=0 ; k<cells-days ; k++){
+                    System.out.printf("    ");
+                    point++;
+                }
+            }
+            for (; day<=days ; day++){
+                System.err.printf("%d ",day);
+                if (point == 7){
+                    System.err.println();
+                    point=0;
+                }
+                point++;
+            }
+        }
+
+        sc.close();
+    }
+    
+}
