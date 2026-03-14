78ef426b3fcf5a740f24664519c59b639ef04bf0
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-03-14 22:21:54 +0530

    Student Grade Management System

---
+++ Arrays/2D Arrays/Problem1.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Problem1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int students = sc.nextInt();
+        int subjects = sc.nextInt();
+
+        int[][] arr = new int[students][subjects];
+
+        for(int i = 0; i < students; i++){
+            for(int j = 0; j < subjects; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Student Averages:");
+
+        for(int i = 0; i < students; i++){
+
+            int sum = 0;
+
+            for(int j = 0; j < subjects; j++){
+                sum += arr[i][j];
+            }
+
+            double avg = (double) sum / subjects;
+
+            System.out.printf("Student %.2f\n", avg);
+        }
+
+        System.out.println();
+        System.out.println("Subject Averages:");
+
+        for(int j = 0; j < subjects; j++){
+
+            int sum = 0;
+
+            for(int i = 0; i < students; i++){
+                sum += arr[i][j];
+            }
+
+            double avg = (double) sum / students;
+
+            System.out.printf("Subject %d : %.2f\n",(j+1), avg);
+        }
+
+        sc.close();
+    }
+}
